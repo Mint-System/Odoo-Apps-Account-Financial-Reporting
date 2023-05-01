c36c73b972be97e36aa4d99d574bffba63c61046
Janik von Rotz <login@janikvonrotz.ch>
2023-05-01 08:50:48 +0200

    [account_sale_timesheet_report] update report

--- account_sale_timesheet_report/__manifest__.py
+++ account_sale_timesheet_report/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Accounting",
-    "version": "15.0.1.6.0",
+    "version": "15.0.1.7.0",
     "license": "AGPL-3",
     "depends": ["account_sale_timesheet"],
     "data": [
