8e1eddbfa134c8623064eefa832c03fc29f803f9
Janik von Rotz <login@janikvonrotz.ch>
2024-02-28 18:25:15 +0100

    feat: init account_reports_hide_ref

--- account_general_ledger_contra_accounts/models/account_general_ledger.py
+++ account_general_ledger_contra_accounts/models/account_general_ledger.py
@@ -1,4 +1,8 @@
+import logging
+
 from odoo import models
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountGeneralLedger(models.AbstractModel):
---
+++ account_reports_hide_ref/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Account Reports Hide Ref",
+    "summary": """
+        Hide journal post ref in general ledger report.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Accounting,",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account_reports"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
