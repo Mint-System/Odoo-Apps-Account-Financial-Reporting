a8936ab0e031928025dfef3f4c115b54f6536983
ulrich.kilian@juleg.ch <ulrich.kilian@juleg.ch>
2024-09-19 08:52:34 +0200

    feat: aged reports rendering

---
+++ account_show_invoice_date/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Account Show Invoice Date",
+    "summary": """
+        Show Invoice date in Account Aged Reports.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Accounting",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account_reports"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "data": [
+        #"views/account_aged_partner.xml"
+    ],
+    "images": ["images/screen.png"],
+}
---
+++ account_show_invoice_date/models/account_aged_partner_balance.py
@@ -0,0 +1,196 @@
+from datetime import datetime
+import logging
+
+from odoo import api, models, fields, _
+from odoo.tools.misc import format_date, get_lang
+
+_logger = logging.getLogger(__name__)
+
+
+
+class ReportAccountAgedPartner(models.AbstractModel):
+    _inherit = 'account.aged.partner'
+    move_invoice_date = fields.Date(string=_('Invoice Date'))
+
+
+    @api.model
+    def _get_sql(self):
+        options = self.env.context['report_options']
+        query = ("""
+            SELECT
+                {move_line_fields},
+                %(sign)s * (
+                    SUM(account_move_line.amount_currency)
+                    - COALESCE(SUM(part_debit.debit_amount_currency), 0)
+                    + COALESCE(SUM(part_credit.credit_amount_currency), 0)
+                ) AS amount_currency,
+                account_move_line.partner_id AS partner_id,
+                partner.name AS partner_name,
+                COALESCE(trust_property.value_text, 'normal') AS partner_trust,
+                COALESCE(account_move_line.currency_id, journal.currency_id) AS report_currency_id,
+                account_move_line.payment_id AS payment_id,
+                COALESCE(account_move_line.date_maturity, account_move_line.date) AS report_date,
+                account_move_line.expected_pay_date AS expected_pay_date,
+                move.move_type AS move_type,
+                move.name AS move_name,
+                move.ref AS move_ref,
+                move.invoice_date AS move_invoice_date,
+                account.code || ' ' || COALESCE(NULLIF(account_tr.value, ''), account.name) AS account_name,
+                account.code AS account_code,""" + ','.join([("""
+                CASE WHEN period_table.period_index = {i}
+                THEN %(sign)s * ROUND((
+                    account_move_line.balance - COALESCE(SUM(part_debit.amount), 0) + COALESCE(SUM(part_credit.amount), 0)
+                ) * currency_table.rate, currency_table.precision)
+                ELSE 0 END AS period{i}""").format(i=i) for i in range(6)]) + """
+            FROM account_move_line
+            JOIN account_move move ON account_move_line.move_id = move.id
+            JOIN account_journal journal ON journal.id = account_move_line.journal_id
+            JOIN account_account account ON account.id = account_move_line.account_id
+            LEFT JOIN res_partner partner ON partner.id = account_move_line.partner_id
+            LEFT JOIN ir_property trust_property ON (
+                trust_property.res_id = 'res.partner,'|| account_move_line.partner_id
+                AND trust_property.name = 'trust'
+                AND trust_property.company_id = account_move_line.company_id
+            )
+            JOIN {currency_table} ON currency_table.company_id = account_move_line.company_id
+            LEFT JOIN LATERAL (
+                SELECT
+                    SUM(part.amount) AS amount,
+                    SUM(part.debit_amount_currency) AS debit_amount_currency,
+                    part.debit_move_id
+                FROM account_partial_reconcile part
+                WHERE part.max_date <= %(date)s
+                GROUP BY part.debit_move_id
+            ) part_debit ON part_debit.debit_move_id = account_move_line.id
+            LEFT JOIN LATERAL (
+                SELECT
+                    SUM(part.amount) AS amount,
+                    SUM(part.credit_amount_currency) AS credit_amount_currency,
+                    part.credit_move_id
+                FROM account_partial_reconcile part
+                WHERE part.max_date <= %(date)s
+                GROUP BY part.credit_move_id
+            ) part_credit ON part_credit.credit_move_id = account_move_line.id
+            JOIN {period_table} ON (
+                period_table.date_start IS NULL
+                OR COALESCE(account_move_line.date_maturity, account_move_line.date) <= DATE(period_table.date_start)
+            )
+            AND (
+                period_table.date_stop IS NULL
+                OR COALESCE(account_move_line.date_maturity, account_move_line.date) >= DATE(period_table.date_stop)
+            )
+            LEFT JOIN ir_translation account_tr ON (
+                account_tr.name = 'account.account,name'
+                AND account_tr.res_id = account.id
+                AND account_tr.type = 'model'
+                AND account_tr.lang = %(lang)s
+            )
+            WHERE account.internal_type = %(account_type)s AND account_move_line.partner_id IS NOT NULL
+            AND account.exclude_from_aged_reports IS NOT TRUE
+            GROUP BY account_move_line.id, partner.id, trust_property.id, journal.id, move.id, account.id,
+                     period_table.period_index, currency_table.rate, currency_table.precision, account_name
+            HAVING ROUND(account_move_line.balance - COALESCE(SUM(part_debit.amount), 0) + COALESCE(SUM(part_credit.amount), 0), currency_table.precision) != 0
+        """).format(
+            move_line_fields=self._get_move_line_fields('account_move_line'),
+            currency_table=self.env['res.currency']._get_query_currency_table(options),
+            period_table=self._get_query_period_table(options),
+        )
+        params = {
+            'account_type': options['filter_account_type'],
+            'sign': 1 if options['filter_account_type'] == 'receivable' else -1,
+            'date': options['date']['date_to'],
+            'lang': self.env.user.lang or get_lang(self.env).code,
+        }
+        return self.env.cr.mogrify(query, params).decode(self.env.cr.connection.encoding)
+
+    # uk: this method must not be overridden
+    @api.model
+    def _get_columns_name(self, options):
+        columns = super(ReportAccountAgedPartner, self)._get_columns_name(options)
+        # Insert  new column header for Invoice Date
+        # columns.insert(1, {'name': 'Rechnungsdatum'})  # Adjust the index as needed
+        return columns
+
+    @api.model
+    def _get_column_details(self, options):
+        columns = super(ReportAccountAgedPartner, self)._get_column_details(options)
+
+        # columns and fields:
+        # report_date = Due Date = Fälligkeit
+        # move_invoice_date = Invoice Date = Rechnungsdatum
+        # account_name = Account = Konto
+        # expected_pay_date = Expected Date = Erwartetes Datum
+
+
+        # do_not_show_columns = [_('Erwartetes Datum'),  _('Konto'), _('Fälligkeit')]
+        do_not_show_columns = [self._field_column('expected_pay_date'), self._field_column('account_name'), self._field_column('report_date')]
+        do_not_show_names = [column.name for column in do_not_show_columns]
+        # due to some reasons in original _get_columns_name account_name is added as Account, so we have Account Name (which we get by field account_name)
+        # and Account which we can't get by field
+        do_not_show_names.append(_('Account'))
+        
+        # remove Columns
+        # columns_without_account_and_date = [column for column in columns if not column.name in do_not_show_columns]
+        columns_without_account_and_date = [column for column in columns if not column.name in do_not_show_names]
+
+        # Insert new column header for Invoice Date
+        columns_without_account_and_date[1:1] = [self._field_column('move_invoice_date')]
+        return columns_without_account_and_date
+
+
+    # uk: this method must not be overridden
+
+    
+    # @api.model
+    # def _get_lines(self, options, line_id=None):
+    #     lines = super(ReportAccountAgedPartner, self)._get_lines(options, line_id)
+    #     # _logger.info('### LINES ###: %s', lines)
+    #     # _logger.info('OPTIONS: %s', options)
+    #     active_lines = []
+    #     for line in lines:
+    #         _logger.info('### LINE: %s', line)
+    #         invoice_date_dict = {}
+    #         id = line.get('id')
+    #         parent_id = line.get('parent_id')
+    #         #if parent_id == 'partner_id-res.partner-False':
+    #         #    continue
+            
+    #         if id: 
+    #          #   if id == 'partner_id-res.partner-False':  
+    #          #       continue
+                    
+    #             if type(id) == int:
+    #                 move_line = self.env['account.move.line'].browse(line['id'])
+    #                 _logger.info("MOVE LINE MOVE ID: %s", move_line.move_id.id)
+    #                 move = self.env['account.move'].search([('id', '=', move_line.move_id.id)], limit=1)
+    #                 # import pdb; pdb.set_trace()
+    #                 if move.invoice_date:
+    #                     invoice_date_dict['name'] = datetime.strftime(move.invoice_date, '%d.%m.%Y')
+    #                     invoice_date_dict['no_format'] = move.invoice_date
+    #                 else:
+    #                     invoice_date_dict['name'] = ''
+    #                     invoice_date_dict['no_format'] = ''
+                    
+    #                 # _logger.info("LINE COLUMN: %s", line['columns'])
+    #                 line['columns'].insert(0, invoice_date_dict)
+    #             else:
+    #                 line['colspan'] += 1
+    #         else:
+    #             line['colspan'] += 1
+
+    #         active_lines.append(line)
+
+
+    #         # print("LINE", line)
+
+    #     # for line in lines:
+    #     #     move_id = line.get('move_id')
+    #     #     if move_id:
+    #     #         move = self.env['account.move'].browse(move_id)
+    #     #         line['invoice_date'] = move.invoice_date or move.date
+
+    #     #_logger.info('get lines: %s', lines)
+
+    #     # return active_lines
+    #     return lines
+
