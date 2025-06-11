3137ffdb7ec9637941120cbf18a98bece523a581
Ulrich Kilian <uk@science-and-more.de>
2025-06-11 17:35:37 +0200

    fix(account_general_ledger_contra_accounts): remove logger line

--- account_general_ledger_contra_accounts/models/account_general_ledger.py
+++ account_general_ledger_contra_accounts/models/account_general_ledger.py
@@ -29,6 +29,5 @@ class AccountGeneralLedger(models.AbstractModel):
                 + "account_move_line.contra_accounts,\n"
                 + full_query[insert_index:]
             )
-            _logger.warning("### query_with_contra_accounts: %s ###" % query_with_contra_accounts)
             return (query_with_contra_accounts, all_params)
         return result
